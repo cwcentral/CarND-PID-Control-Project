e0fb1c02180263511e4e1de0070ef3f1875af8f7
cwcentral <cwcentral@aol.com>
2018-09-16 14:34:32 -0700

    final

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
-*/
-PID::PID() {}
+ * TODO: Complete the PID class.
+ */
+PID::PID() {
+}
 
-PID::~PID() {}
+PID::~PID() {
+}
 
 void PID::Init(double Kp, double Ki, double Kd) {
 	this->Kp = Kp;
@@ -18,23 +20,28 @@ void PID::Init(double Kp, double Ki, double Kd) {
 	this->d_error = 0.;
 }
 void PID::UpdateError(double error) {
-	    static double last_error = 0;
-        static double i_sum = 0;
 
-        // proportional
-	    p_error = Kp * error;
+	// See lesson 12
 
-	    // derivative
-	    d_error = Kd * (error - last_error);
-		last_error = error;
+	static double last_error = 0;
+	static double i_sum = 0;
 
-		// integral
-		i_sum += error;
-		i_error = Ki * i_sum;
+	// proportional
+	p_error = Kp * error;
+
+	// derivative
+	d_error = Kd * (error - last_error);
+	last_error = error;
+
+	// integral
+	i_sum += error;
+	i_error = Ki * i_sum;
 
 }
 
 double PID::TotalError() {
+
+	// lesson 11
 	return p_error + i_error + d_error;
 }
 
